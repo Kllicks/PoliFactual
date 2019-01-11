86f6db8988baf35e2b0d841f3b9f1b1350f37512
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2019-01-11 09:42:14 -0500

    adds dotenv package to backend

--- client/src/components/LocalComponent.js
+++ client/src/components/LocalComponent.js
@@ -22,16 +22,36 @@ class Locals extends Component {
   }
 
   componentDidMount() {
-    fetch(
-      "https://www.googleapis.com/civicinfo/v2/representatives?address=1554%20line%20st.%2030032&includeOffices=true&key=AIzaSyB3cRW6zO8D3INc-NHDFA-0ck77gQAYpOU",
+    let apiRequest1 = fetch(
+      "https://www.googleapis.com/civicinfo/v2/representatives?address=1554%20line%20st.%2030032&includeOffices=true&key=git",
       { headers: { "Content-Type": "application/json; charset=utf-8" } }
-    )
-      .then(response => response.json())
-      .then(results => {
-        let newResults = Object.values(results); // newResults will be json response array of the users civic representives at each elected level of government.
+    ).then(response => response.json());
+
+    let apiRequest2 = fetch(
+      "http://www.opensecrets.org/api/?method=getLegislators&id=NJ&",
+      {
+        headers: { apikey: "cd6516f049350cdb9ffc4264527af56e" }
+      }
+    ).then(response => response.json());
+
+    let combinedData = { apiRequest1: {}, apiRequest2: {} };
+
+    Promise.all([apiRequest1, apiRequest2])
+      .then(function(values) {
+        combinedData["apiRequest1"] = values[0];
+        combinedData["apiRequest2"] = values[1];
+        return combinedData;
+      })
+
+      .then(combinedData => {
+        let newResults = Object.values(combinedData.apiRequest1); // newResults is the json response array of the users civic representives at each elected level of government.
+        let otherResults = Object.values(combinedData.apiRequest2); // otherResults will be json response array of the users civic representives at each elected level of government.
         console.log(newResults);
+        console.log(otherResults);
         let officesArray = newResults[3]; // officesArray equals the names of the office and info about that office for each level of elected government and its
         console.log(officesArray);
+        console.log(officesArray[0].divisionId);
+        console.log("hi");
         console.log(newResults[4]);
         let personInfoArray = newResults[4]; // personInfoArray is the names and info for the person who currently has been elected to the seat of the newResults array.
         let masterArray = [];
@@ -47,7 +67,7 @@ class Locals extends Component {
             let personEmail = personInfo.emails || null; // if an elected official has an email address add that value to personEmail.
             let personPhoto = personInfo.photoUrl || null; //if an elected official has a photo url add that value to personPhoto
             let personUrl = personInfo.urls || null; // if an elected official has a website (personal or for the department) add that value to personUrl
-
+            console.log(personInfo);
             console.log(personUrl);
             if (personInfo.address) {
               if (personInfoArray[index].channels) {
@@ -82,82 +102,86 @@ class Locals extends Component {
         this.setState({
           personOfficeInfo: masterArray
         });
+      })
+      .catch(function(error) {
+        console.log("Looks like there was a problem: \n", error);
       });
-
-    // fetch(
-    //   "https://www.googleapis.com/civicinfo/v2/elections?&key=AIzaSyB3cRW6zO8D3INc-NHDFA-0ck77gQAYpOU",
-    //   { headers: { "Content-Type": "application/json; charset=utf-8" } }
-    // )
-    //   .then(response => response.json())
-    //   .then(data => console.log(data));
   }
 
   render() {
     let officeNames = this.state.personOfficeInfo.map(function(item, index) {
       return (
-        <Col>
-          <Card style={{ width: "20rem" }}>
-            {/* <CardImage
-              className="img-fluid"
-              src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg"
-              /> */}
-            <CardBody className="h-100 w-100">
-              <ul key={index}>
-                <CardTitle>
-                  {item.photo ? (
-                    <div>
-                      <img src={item.photo} alt="" />
-                    </div>
-                  ) : null}
-                  {item.personName ? <div>{item.personName}</div> : null}
-                  <br />
-                  {item.officeName ? <div>{item.officeName}</div> : null}
-                  <br />
-                </CardTitle>
-                <CardText>
-                  {item.address.line1 ? <div>{item.address.line1}</div> : null}
-                  {item.address.line2 ? <div>{item.address.line2}</div> : null}
-                  {item.address.city ? <div>{item.address.city}</div> : null}
-                  {item.address.state ? <div>{item.address.state}</div> : null}
-                  {item.address.zip ? <div>{item.address.zip}</div> : null}
-                  {item.party ? <div>{item.party}</div> : null}
-                  {item.phoneNumber ? <div>{item.phoneNumber}</div> : null}
-                  {item.url ? <a href={item.url}>{item.url}</a> : null}
-                  {item.email ? (
-                    <a href={"mailto:" + item.email}>{item.email}</a>
-                  ) : null}
-                  {item.twitter ? (
-                    <div>
-                      <Timeline
-                        dataSource={{
-                          sourceType: "profile",
-                          screenName: item.twitter
-                        }}
-                        options={{
-                          username: item.twitter,
-                          height: "400",
-                          width: "60%"
-                        }}
-                        onLoad={() => console.log("Timeline is loaded!")}
-                      />
-                    </div>
-                  ) : null}
-                  <div />
-                </CardText>
-              </ul>
-              <MDBContainer>
-                <MDBBtn size="lg" tag="a" floating social="email">
-                  <MDBIcon icon="envelope" />
-                </MDBBtn>
-              </MDBContainer>
-            </CardBody>
-          </Card>
-        </Col>
+        <div>
+          <Col>
+            <Card style={{ width: "20rem" }}>
+              {/* <CardImage
+                className="img-fluid"
+                src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg"
+                /> */}
+              <CardBody className="h-100 w-100">
+                <ul key={index}>
+                  <CardTitle>
+                    {item.photo ? (
+                      <div>
+                        <img src={item.photo} alt="" />
+                      </div>
+                    ) : null}
+                    {item.personName ? <div>{item.personName}</div> : null}
+                    <br />
+                    {item.officeName ? <div>{item.officeName}</div> : null}
+                    <br />
+                  </CardTitle>
+                  <CardText>
+                    {item.address.line1 ? (
+                      <div>{item.address.line1}</div>
+                    ) : null}
+                    {item.address.line2 ? (
+                      <div>{item.address.line2}</div>
+                    ) : null}
+                    {item.address.city ? <div>{item.address.city}</div> : null}
+                    {item.address.state ? (
+                      <div>{item.address.state}</div>
+                    ) : null}
+                    {item.address.zip ? <div>{item.address.zip}</div> : null}
+                    {item.party ? <div>{item.party}</div> : null}
+                    {item.phoneNumber ? <div>{item.phoneNumber}</div> : null}
+                    {item.url ? <a href={item.url}>{item.url}</a> : null}
+                    {item.email ? (
+                      <a href={"mailto:" + item.email}>{item.email}</a>
+                    ) : null}
+                    {item.twitter ? (
+                      <div>
+                        <Timeline
+                          dataSource={{
+                            sourceType: "profile",
+                            screenName: item.twitter
+                          }}
+                          options={{
+                            username: item.twitter,
+                            height: "400",
+                            width: "60%"
+                          }}
+                          onLoad={() => console.log("Timeline is loaded!")}
+                        />
+                      </div>
+                    ) : null}
+                    <div />
+                  </CardText>
+                </ul>
+                <MDBContainer>
+                  <MDBBtn size="lg" tag="a" floating social="email">
+                    <MDBIcon icon="envelope" />
+                  </MDBBtn>
+                </MDBContainer>
+              </CardBody>
+            </Card>
+          </Col>
+        </div>
       );
     });
     return (
       <div className="Main">
-        <p>{officeNames}</p>
+        <div>{officeNames}</div>
       </div>
     );
   }
