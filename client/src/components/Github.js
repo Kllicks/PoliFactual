669145cf95a3e0ad8f6247e509bb6a9b8f3ed1bc
hharris0803 <hchang0803@outlook.com>
2019-01-14 17:41:30 -0500

    add styling to github and trello page:

--- client/src/components/Github.js
+++ client/src/components/Github.js
@@ -6,29 +6,30 @@ import Styles from '../styles/Github.css';
 const Github = () => {
   return (
     <div>
-      <section className="text-center my-5">
+      <section className="text-center my-5 px-5 pt-5 pb-5">
         <MDBRow>
           <MDBCol md="12" className="mb-4">
             <MDBCard
-              className="card-image"
-              style={{
-                backgroundImage:
-                  "url(https://mdbootstrap.com/img/Photos/Others/img%20%2832%29.jpg)"
-              }}
+              // className="card-image"
+              // style={{
+              //   backgroundImage:
+              //     "url(https://mdbootstrap.com/img/Photos/Others/img%20%2832%29.jpg)"
+              // }}
+
             >
-              <div className="text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4">
+              <div className="text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4 white">
                 <div>
-                  <h2 className="purple-text py-3 font-weight-bold">
+                  <h2 className="indigo-text py-3 font-weight-bold">
                     <MDBIcon icon="github" size="lg" />
                     <br />
                     <strong>Github</strong>
                   </h2>
-                  <p className="pb-3">
+                  <p className="pb-3 grey-text">
                     Our group decided to use Github to make it easy to collaborate and look back at previous versions of our work. 
                     Please check out this projects repo on Github!
                     Dont forget to also check out our Trello Board! 
                   </p>
-                  <MDBBtn color="secondary" rounded size="md" href="https://github.com/Kllicks/Polifactual">
+                  <MDBBtn color="indigo" rounded size="md" href="https://github.com/Kllicks/Polifactual">
                     <MDBIcon icon="clone" className="left"/>  See Github for Project
                   </MDBBtn>
                 </div>
--- client/src/components/Trello.js
+++ client/src/components/Trello.js
@@ -7,8 +7,8 @@ import Styles from '../styles/Trello.css';
 
 const Trello = () => {
   return (
-
-    <MDBCard className="my-5 px-5 pt-5 pb-5 white">
+    <MDBContainer className="my-5 px-5 pt-5 pb-5 white">
+    <MDBCard >
         <MDBRow>
           <MDBCol md="12">
             <MDBCard>
@@ -22,20 +22,19 @@ const Trello = () => {
                 <h1 className="font-weight-bold my-5">
                   Trello
                 </h1>
+                <p className="grey-text">
+                Our team chose to use Trello as our choice of project planning software. We chose to use Trello to manage our web project for two reasons. It is free and helps keep us organized and focused. 
+              </p>
                 <MDBBtn className="btn-fb waves-light" color="indigo darken-4">
                   <MDBIcon icon="trello" />
                   <a href="https://trello.com/b/pipJjSew/capstone-project"><p className="white-text">Click here to check out our Trello page!</p></a>
                 </MDBBtn>
               </MDBCardBody>
             </MDBCard>
-            <MDBContainer className="mt-5">
-              <p className="grey-text">
-                Our team chose to use Trello as our choice of project planning software. We chose to use Trello to manage our web project for two reasons. It is free and helps keep us organized and focused. 
-              </p>
-            </MDBContainer>
           </MDBCol>
         </MDBRow>
     </MDBCard>
+    </MDBContainer>
   );
 }
 
