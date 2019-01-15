2d988c76d38198e64daa759ae05f7a2c5edc646e
hharris0803 <hchang0803@outlook.com>
2019-01-14 19:29:11 -0500

    add css to footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -17,19 +17,64 @@ import {
     return (
     <Footer color="indigo darken-4 gradient" className="font-small pt-4 mt-4">
       <Container fluid className="text-center text-md-center">
-        <Row>
-          <Col md="12" className="indigo darken-3 footer">
-              <a href="#!">Help</a>
-              <a href="#!">Jobs</a>
-              <a href="#!">Terms</a>
-              <a href="#!">Privacy</a>
-          </Col>
-        </Row>
+      <Row>
+      <Col md="5">
+      <h5 className="title indigo darker-4">Polifactual</h5>
+      <p>
+        Polifactual was a capstone project created by four DigitalCrafts Students. 
+      </p>
+      </Col>
+      <Col md="2">
+      <h5 className="title indigo darker-4">Help</h5>
+      <ul>
+        <li className="list-unstyled">
+          <a href="#!">Contact Us</a>
+        </li>
+        <li className="list-unstyled">
+          <a href="#!">Client Services</a>
+        </li>
+        <li className="list-unstyled">
+          <a href="#!">Site Map</a>
+        </li>
+        <li className="list-unstyled">
+          <a href="#!">Privacy Policy</a>
+        </li>
+        <li className="list-unstyled">
+          <a href="#!">Terms of Use</a>
+        </li>
+      </ul>
+      </Col>
+      <Col md="2">
+      <h5 className="title indigo darker-4">About</h5>
+      <ul>
+        <li className="list-unstyled">
+          <a href="#!">About Us</a>
+        </li>
+        <li className="list-unstyled">
+          <a href="#!">Careers</a>
+        </li>
+        <li className="list-unstyled">
+          <a href="#!">Meet the Team</a>
+        </li>
+      </ul>
+      </Col>
+      <Col md="2">
+      <h5 className="title indigo darker-4">Follow Us</h5>
+      <ul>
+        <li className="list-unstyled">
+          <a href="#!">Github</a>
+        </li>
+        <li className="list-unstyled">
+          <a href="#!">Trello</a>
+        </li>
+      </ul>
+      </Col>
+    </Row>
       </Container>
       <div className="footer-copyright text-center py-3">
         <Container fluid>
-          &copy; {new Date().getFullYear()} Copyright:{" "}
-          <p> Helen, Kyle, Brien, Isaac Fonseca </p>
+          <h5>&copy; {new Date().getFullYear()} Copyright:{" "}
+          Helen, Kyle, Brien, Isaac Fonseca . All Rights Reserved</h5>
         </Container>
       </div>
     </Footer>
